3545ff6e7ae578714d466560efdcfa31ab03d4a6
Dmytro Ivanchenko <brutalhulk@gmail.com>
2020-12-09 13:24:15 +0200

    new homework

--- src/Employee.java
+++ src/Employee.java
@@ -1,36 +1,34 @@
-public class Employee {
-  // TODO fix class declaration and declare variables here
+public abstract class Employee implements Payable{
+  private String emploeeId;
+  private  String name;
 
   public Employee(String employeeId, String name) {
-    // TODO fill in code here
+    this.emploeeId = employeeId;
+    this.name = name;
   }
 
   public String getEmployeeId() {
-    // TODO fill in code here and replace the return statement
-    return "";
+    return this.emploeeId;
   }
 
   public void setEmployeeId(String employeeId) {
-    // TODO fill in code here
+    this.emploeeId = employeeId;
   }
 
   public String getName() {
-    // TODO fill in code here and replace the return statement
-    return "";
+    return this.name;
   }
 
   public void setName(String name) {
-    // TODO fill in code here
+    this.name = name;
   }
 
   public double getAverageMonthlySalary() {
-    // TODO fill in code here and replace the return statement
-    return 0;
+    return calculatePay();
   }
 
   @Override
   public String toString() {
-    // TODO fill in code here and replace the return statement, be sure to format double value
-    return "";
+    return this.name + " " + this.emploeeId + "" + String.format("%.2f",calculatePay());
   }
 }
